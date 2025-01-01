a64c6233771b1dc472323777a7393931c20f9f23
Efe Emmanuel <83638482+efeemmanuel@users.noreply.github.com>
2025-01-01 19:55:50 +0100

    Update serializers.py

--- api/appapi/serializers.py
+++ api/appapi/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 
 
-class UserSerializer(serializers.ModelSerializer)
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username','first_name','last_name','email','password']
