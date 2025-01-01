80fd5068d139d8c7f5986ecdc880448e96b24bb6
Efe Emmanuel <83638482+efeemmanuel@users.noreply.github.com>
2025-01-01 19:56:55 +0100

    Update permissions.py

--- api/appapi/permissions.py
+++ api/appapi/permissions.py
@@ -1,1 +1,14 @@
 
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+    
+
+class IsOwnerOrAdmin(BasePermission):
+    """
+    Custom permission to allow only the creator or an admin to delete or update.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Allow if the user is the creator or an admin
+        return obj.creator == request.user or request.user.is_staff
